e1e0b5fca06315b3146947d9d8c981d8b5ac6057
Moorlack <sych.worker@gmail.com>
2023-10-27 10:01:34 +0300

    readme+start page

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# backend
+# How to run it locally
+
+1. Open project with terminal or IDE
+2. docker-compose build
+3. docker-compose up
+4. to exit: docker-compose down or ctrl+c
--- mymodule/views.py
+++ mymodule/views.py
@@ -4,3 +4,8 @@ from mymodule import app
 @app.route('/healthcheck')
 def healthcheck():
     return jsonify({'status': 'ok'})
+
+@app.route("/")
+def homepage():
+    response = "<h1>Back-end goes brrr</h1>"
+    return response, 200
